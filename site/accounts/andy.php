d858841c9e92c998e11e9f2dfb6a73686102ff7a
Andrew Grosser <dioptre@gmail.com>
2015-04-12 17:10:50 -0700

    updated site title

--- site/accounts/andy.php
+++ site/accounts/andy.php
@@ -6,6 +6,7 @@ password: >
   $2a$10$zl/uU6EjztdFuYmrpm/soOSleVyijyb1VTwl.4L/YiZfl8wSe9F4O
 language: en
 history:
+  - null
   - home
 token: 336b0522c09f3b30a4b5d6bc3a65f879c4cacb45
 firstname: Andy
